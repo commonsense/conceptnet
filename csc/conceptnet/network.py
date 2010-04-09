cecfa41d5281ef0938de587c8df6c0f780f80e54
Rob Speer <rspeer@mit.edu>
2010-04-09 14:27:54 -0400

    Changed network.py to export in edgelist format (easier to parse than GML)

--- csc/conceptnet/network.py
+++ csc/conceptnet/network.py
@@ -17,11 +17,19 @@ def make_network(lang):
     for text1, text2, rel, score, freq in assertions.values_list(
         'concept1__text', 'concept2__text', 'relation__name', 'score',
         'frequency__value').iterator():
-        graph.add_edge(text1, text2, key=rel, score=score, freq=freq)
+        if text1 and text2:
+            graph.add_edge(text1, text2, rel=rel, score=score, freq=freq)
     return graph
 
-def export_network(lang, filename):
+def export_gml(lang, filename):
     f = codecs.open(filename, 'w', encoding='utf-7')
     graph = make_network(lang)
     nx.write_gml(graph, f)
     f.close()
+
+def export_edgelist(lang, filename):
+    f = codecs.open(filename, 'w', encoding='utf-8')
+    graph = make_network(lang)
+    nx.write_edgelist(graph, f, data=True, delimiter='\t')
+    f.close()
+
