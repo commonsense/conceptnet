d8238cedffab4400f75764036b4cf3c0db1798c6
Kenneth Arnold <kcarnold@alum.mit.edu>
2010-05-27 21:40:29 -0400

    Add a probable fix for py2exe.

--- csc/__init__.py
+++ csc/__init__.py
@@ -1,2 +1,6 @@
 # The following line MUST be the only line in this file, according to the setuptools docs:
 __import__('pkg_resources').declare_namespace(__name__)
+# But you also need the following for py2exe to work, according to http://www.py2exe.org/index.cgi/ExeWithEggs
+import modulefinder
+for p in __path__:
+   modulefinder.AddPackagePath(__name__, p)
